215be5bbd0a57cf278f2c5a6a2e2f933b3efd584
Sergey <lolito0700@mail.ru>
2023-02-21 17:06:59 +0300

    zad4

---
+++ zad4/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите сумму вклада: ");
+decimal Sum = decimal.Parse(Console.ReadLine());
+Console.Write("Введите количество лет: ");
+int year = int.Parse(Console.ReadLine());
+ 
+int x = 1;
+ 
+while (x <= year)
+{
+  if (Sum < 100)
+  {
+    Sum = Sum + (Sum * (decimal)0.05);
+  }
+  if ((Sum > 100) && (Sum < 200))
+ {
+    Sum = Sum + (Sum * (decimal)0.07);
+  }
+  if (Sum > 200)
+  {
+        Sum = Sum + (Sum * (decimal)0.1);
+  }
+ 
+  x++;
+}
+ 
+Console.WriteLine($"Cумму вклада с начисленными процентами за {year} лет составит: {Sum}");
+Console.ReadKey();
